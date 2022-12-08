b11108b9bac378b57203739e63c516b684aa7d34
dobinh3212 <95838086+dobinh3212@users.noreply.github.com>
2022-12-08 15:54:57 +0700

    update project laravel docker

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+# composer install
+composer install 
+
+#convert env.example
+cp .env.example .env
+
+# Apply database migrations
+php artisan migrate 
+
+# create key generate
+php artisan key:generate
+
+# Start server
+php artisan serve --host 0.0.0.0
