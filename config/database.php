03e44866a5314552da70ea71c5e482a7b3664cd8
dobinh3212 <95838086+dobinh3212@users.noreply.github.com>
2022-12-16 09:45:55 +0700

    config database test

--- config/database.php
+++ config/database.php
@@ -62,7 +62,18 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-
+        //cấu hình kết nối data test
+        'mysql_test' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST_TEST', '127.0.0.1'),
+            'database'  => env('DB_DATABASE_TEST', 'forge'),
+            'username'  => env('DB_USERNAME_TEST', 'forge'),
+            'password'  => env('DB_PASSWORD_TEST', ''),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
